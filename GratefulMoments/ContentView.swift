968540a1f011019a26629a3d596591e992ea058b
Phong Hy <doivjpxx@gmail.com>
2025-11-29 13:49:08 +0700

    Initial Commit

---
+++ GratefulMoments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GratefulMoments
+//
+//  Created by Phong Hy on 29/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Phong Hy on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
